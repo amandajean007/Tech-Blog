5ad83db30230582e6e481c046d6d324958df30bd
Amanda McBee <ahanes35@yahoo.com>
2021-10-13 14:15:11 -0600

    initial commit even though halfway worked on oops

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,73 @@
+const router = require('express').Router();
+const { Post } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+// ('api/post')
+
+// create a post
+router.post('/', withAuth, async (req, res) => {
+  try {
+    console.log("line 10 postRoutes / create a post");
+    const newPost = await Post.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+    res.status(200).json(newPost)
+    console.log(newPost);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// find post by id
+router.get('/post/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const Post = postData.get({ plain: true });
+
+    res.render('Post', {
+      ...Post,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// comment on a post
+// router.post('/post/:id', withAuth, async (req, res) => {
+//   try {
+//     const 
+//   }
+// })
+
+// Delete post
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: 'No post found with this id!' });
+      return;
+    }
+
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
---
+++ controllers/homeRoutes.js
@@ -0,0 +1,61 @@
+const router = require('express').Router();
+const { Post, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+// ('/')
+
+// homepage // Get all Posts and JOIN with user data
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('homepage', { 
+      posts, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.redirect('/login');
+  }
+});
+
+// Find the logged in user based on the session ID
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Post }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('profile', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Log in and Sign up
+// If the user is already logged in, redirect the request to another route
+router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    // res.redirect('/profile');
+    return;
+  }
+  res.render('login');
+});
+
+module.exports = router;
