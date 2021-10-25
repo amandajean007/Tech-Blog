f3f685c7f1901a5acbf8fae7aa3d25ea520cf09b
Amanda McBee <ahanes35@yahoo.com>
2021-10-25 14:57:58 -0600

    comment route construction

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -4,20 +4,38 @@ const withAuth = require('../../utils/auth');
 
 // ('/api/comment/')
 
-// Get all comments
-router.get('/', withAuth, async (req, res) => {
-    console.log("is anyone out there?");
-try {
-    const commentData = await Comment.findAll();
-    if (!commentData) {
-        res.status(404).json({message: 'No comments found'});
-        return;
+// post a comment
+router.post("/", withAuth, async (req, res) => {
+    try {
+      const newComment = await Comment.create({
+        ...req.body,
+        user_id: req.session.user_id,
+      });
+  
+      res.status(200).json(newComment);
+    } catch (err) {
+      res.status(400).json(err);
     }
+  });
+
+// delete comment
+router.delete("/:id", withAuth, async (req, res) => {
+try {
+    const commentData = await Comment.destroy({
+    where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+    },
+    });
+
+    if (!commentData) {
+    res.status(404).json({ message: "No comment found with this id!" });
+    return;
+    }
+
     res.status(200).json(commentData);
-    
 } catch (err) {
     res.status(500).json(err);
-    console.log("or over here?");
 }
 });
 
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -75,7 +75,7 @@ router.get('/profile', withAuth, async (req, res) => {
 // If the user is already logged in, redirect the request to another route
 router.get('/login', (req, res) => {
   if (req.session.logged_in) {
-    // res.redirect('/profile');
+    res.redirect('/profile');
     return;
   } else {
   res.render('login');
